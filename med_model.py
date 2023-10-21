aed4a09dea66a2e86a5158d8b0e7a84e2e5632c6
JINAILAB <parksonice@naver.com>
2023-10-22 00:36:51 +0900

    [Feat] resnet 모델 추가

--- med_model.py
+++ med_model.py
@@ -1,13 +1,10 @@
 import monai
-from monai.networks.blocks import ResBlock
-from monai.networks.layers.factories import Dropout
+from resnet3d import ResNet3dClassification
+
 
 densenet_model = monai.networks.nets.DenseNet121(spatial_dims=3, in_channels=1, out_channels=2)
+resnet_model = ResNet3dClassification()
 
 
 
-
-
-
-### segresnet for classifcation
 
